4bd33a8aa9307ce75b172f382feb191b73eb6b2b
Citra Apsara <putu.citra.a@mail.ugm.ac.id>
2022-02-08 10:25:46 +0800

    kapital

--- application/controllers/Datapengguna.php
+++ application/controllers/Datapengguna.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class DataPengguna extends CI_Controller {
+class Datapengguna extends CI_Controller {
 
 	public function index()
 	{
